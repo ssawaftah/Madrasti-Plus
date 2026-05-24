419310b34ae0f0f4a82411c68f317f124d7d9eff
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2026-05-24 05:33:41 +0300

    Add Firestore database id config

--- lib/core/config/firebase_config.dart
+++ lib/core/config/firebase_config.dart
@@ -4,4 +4,8 @@ class FirebaseConfig {
   static const bool isEnabled = true;
 
   static const String defaultSchoolId = 'school_001';
+
+  // Cloud Firestore database ID. The default Firebase database ID is "(default)".
+  // If you created a named Firestore database, replace this value with its ID.
+  static const String firestoreDatabaseId = '(default)';
 }
