2316de1aafd5723888331b15860b4a4162b6c59d
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2026-05-24 04:41:49 +0300

    Add Firebase config flag

---
+++ lib/core/config/firebase_config.dart
@@ -0,0 +1,11 @@
+class FirebaseConfig {
+  const FirebaseConfig._();
+
+  // Change this to true after running FlutterFire configuration locally.
+  // Required files:
+  // - lib/firebase_options.dart
+  // - android/app/google-services.json
+  static const bool isEnabled = false;
+
+  static const String defaultSchoolId = 'school_001';
+}
